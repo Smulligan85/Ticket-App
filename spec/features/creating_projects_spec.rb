baa6169846873014af1df5919596b00ccd334833
Sean Mulligan <sean.mulligan85@gmail.com>
2015-09-29 07:26:42 -0700

    Adding index new create show and project params

---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new projects" do
+  scenario "with valid attributes" do
+    visit "/"
+    click_link "New Project"
+
+    fill_in "Name", with: "The Visit"
+    fill_in "Description", with: "New Horror Movie"
+    click_button "Create Project"
+
+    expect(page).to have_content "Movie has been created"
+  end
+end
