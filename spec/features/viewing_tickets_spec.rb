513b3a4ff02d749f2e4fb65db3020abcaa28fed4
Sean Mulligan <sean.mulligan85@gmail.com>
2015-10-06 09:56:15 -0700

    Add ticket view feature

---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Users can view tickets" do
+  before do
+    sublime = FactoryGirl.create(:project, name: "Sublime Text 3")
+    FactoryGirl.create(:ticket, project: sublime, 
+      name: "Make it new!", 
+      description: "New Sublime Text 3 code editor.")
+
+    ie = FactoryGirl.create(:project, name: "Internet Explorer")
+    FactoryGirl.create(:ticket, project: ie, 
+      name: "Standards compliance",
+      description: "Isn't a joke")
+
+    visit "/"
+  end
+
+  scenario "for a given project" do
+    click_link "Sublime Text 3"
+
+    expect(page).to have_content "Make it new!"
+    expect(page).to_not have_content "Standards compliance"
+
+    click_link "Make it new!"
+    within("#ticket h2") do
+      expect(page).to have_content "Make it new!"
+    end
+
+    expect(page).to have_content "New Sublime Text 3 code editor."
+  end
+end
