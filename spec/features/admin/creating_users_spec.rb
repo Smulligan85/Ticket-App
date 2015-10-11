a89b528ab84c475f4d9dc98acca3813c481d4618
Sean Mulligan <sean.mulligan85@gmail.com>
2015-10-10 20:11:23 -0700

    Add feature to create admin users

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -11,10 +11,20 @@ RSpec.feature "Admin users can create new users" do
     click_link "New Users"
   end
 
-  scenario "with vallid attributes" do
+  scenario "with valid attributes" do
     fill_in "Email", with: "newbie@example.com"
     fill_in "Password", with: "password"
     click_button "Create User"
     expect(page).to have_content "User has been created."
   end
+
+  scenario "create admin user" do
+    fill_in "Email", with: "fooadmin@example.com"
+    fill_in "Password", with: "password"
+    check "Is an admin?"
+    click_button "Create User"
+    expect(page).to have_content "User has been created."
+    expect(page).to have_content "fooadmin@example.com (Admin)"
+  end
+  
 end 
